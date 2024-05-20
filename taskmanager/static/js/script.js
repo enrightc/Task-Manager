49743f56659a100143a1360e4daac7529a737e1c
Charles Enright <c.enright@me.com>
2024-05-20 16:42:46 +0000

    Feat: Add template to allow users to view tasks from the database

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -3,16 +3,18 @@ document.addEventListener("DOMContentLoaded", function() {
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
 
-    // datapicker initialization
-    let datepicker = document.querySelectorAll('.datepicker');
+    // datepicker initialization
+    let datepicker = document.querySelectorAll(".datepicker");
     M.Datepicker.init(datepicker, {
         format: "dd mmmm, yyyy",
         i18n: {done: "Select"}
     });
 
     // select initialization
-    let selects = document.querySelectorAll('select');
+    let selects = document.querySelectorAll("select");
     M.FormSelect.init(selects);
+
+    // collapsible initializataion
+    let collapsibles = document.querySelectorAll(".collapsible");
+    M.Collapsible.init(collapsibles);
 });
-
-
